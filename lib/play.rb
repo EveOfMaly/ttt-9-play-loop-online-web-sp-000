8628da9401c2c30dd35fdb2ff4ba21d2ced45e91
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-04-14 01:54:45 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -16,7 +16,7 @@ def move(board, index, current_player = "X")
 end
 
 def position_taken?(board, location)
-  ≈
+  board[location] != " " && board[location] != ""
 end
 
 def valid_move?(board, index)
